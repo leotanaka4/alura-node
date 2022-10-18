1756706a9e832c40a886a9251ca1035fc68cbb81
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2022-10-18 20:11:33 -0300

    Relação e Busca

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,23 +2,29 @@ import livros from "../models/Livro.js";
 
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
-      res.status(200).json(livros);
-    });
+    livros
+      .find()
+      .populate("autor")
+      .exec((err, livros) => {
+        res.status(200).json(livros);
+      });
   };
 
   static listarLivroPorId = (req, res) => {
     const id = req.params.id;
 
-    livros.findById(id, (err, livro) => {
-      if (err) {
-        res
-          .status(400)
-          .send({ message: `${err.message} - Id do livro não localizado.` });
-      } else {
-        res.status(200).send(livro);
-      }
-    });
+    livros
+      .findById(id)
+      .populate("autor", "nome")
+      .exec((err, livros) => {
+        if (err) {
+          res
+            .status(400)
+            .send({ message: `${err.message} - Id do livro não localizado.` });
+        } else {
+          res.status(200).send(livros);
+        }
+      });
   };
 
   static cadastrarLivro = (req, res) => {
@@ -46,15 +52,27 @@ class LivroController {
       }
     });
   };
-  
+
   static excluirLivro = (req, res) => {
     const id = req.params.id;
 
     livros.findByIdAndDelete(id, (err) => {
-      if(!err) {
+      if (!err) {
         res.status(200).send({ message: "Livro excluído com sucesso!" });
       } else {
         res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
+  static listarLivrosPorEditora = (req, res) => {
+    const editora = req.query.editora;
+
+    livros.find({ editora: editora }, {}, (err, livros) => {
+      if (err) {
+        res.status(500).send({ message: err.message });
+      } else {
+        res.status(200).send(livros);
       }
     });
   };
